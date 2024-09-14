45ac6439fa468f3686042eaa38bd95b531666868
Manish Shrestha <18145348+manishshres@users.noreply.github.com>
2024-09-14 11:14:20 -0400

    implemented backend source code

---
+++ src/main/java/dev/capestone/jwtauth/service/UserService.java
@@ -0,0 +1,35 @@
+package dev.capestone.jwtauth.service;
+
+import dev.capestone.jwtauth.model.User;
+import dev.capestone.jwtauth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User createUser(User user) {
+        // Check if user already exists
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email is already in use!");
+        }
+
+        // Encode password
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        // Save user
+        return userRepository.save(user);
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found!"));
+    }
+}
